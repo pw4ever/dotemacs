f3031eec7cef0061dadc6d663681781e5c6c0b13
Wei Peng <write.to.peng.wei@gmail.com>
2014-09-27 15:11:43 -0400

    install -> push; add sync- version of push/pull

--- sync-pull.sh
+++ sync-pull.sh
@@ -3,5 +3,5 @@
 # http://stackoverflow.com/a/246128/1527494
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp -v ~/.emacs ${DIR}/.emacs
-cp -vr ~/.emacs.d/config ${DIR}/.emacs.d/
+rm -rf ${DIR}/.emacs.d/config/
+${DIR}/pull.sh
--- sync-push.sh
+++ sync-push.sh
@@ -3,5 +3,5 @@
 # http://stackoverflow.com/a/246128/1527494
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp -v ${DIR}/.emacs ~/.emacs
-cp -vr ${DIR}/.emacs.d ~
+rm -rf ~/.emacs.d/config/
+${DIR}/push.sh
