9bc25806eabc8d8eeb551171114cd5191d2b3085
Wei Peng <write.to.peng.wei@gmail.com>
2014-09-27 13:15:28 -0400

    pull.sh: sync repo to local config

---
+++ pull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash - 
+
+# http://stackoverflow.com/a/246128/1527494
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp -v ~/.emacs ${DIR}/.emacs
+cp -vr ~/.emacs.d/config/* ${DIR}/.emacs.d/config
