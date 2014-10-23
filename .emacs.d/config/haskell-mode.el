728660e6b808bc63b75cc7bbf5228fd4590ece3d
Wei Peng <write.to.peng.wei@gmail.com>
2014-10-23 10:59:40 -0400

    update haskell-mode config based on its github wiki

--- .emacs.d/config/haskell-mode.el
+++ .emacs.d/config/haskell-mode.el
@@ -1,31 +1,45 @@
+(eval-after-load "which-func"
+  '(add-to-list 'which-func-modes 'haskell-mode))
+
+;; https://github.com/haskell/haskell-mode/wiki/Haskell-Interactive-Mode-Setup
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-unicode-input-method)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
-(eval-after-load "which-func"
-  '(add-to-list 'which-func-modes 'haskell-mode))
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
 
 (eval-after-load "haskell-mode"
   '(progn
      (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
-     (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
-
-(eval-after-load "haskell-mode"
-  '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
-
-(eval-after-load "haskell-cabal"
-  '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
-
-(eval-after-load "haskell-mode"
-  '(progn
-     (define-key haskell-mode-map (kbd "C-x C-d") nil)
-     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)  
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+     (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
      (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
      (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-     (define-key haskell-mode-map (kbd "C-c M-.") nil)
-     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+     (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+     (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+     (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+     (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+
 
 (eval-after-load "haskell-cabal-mode"
-  (define-key haskell-mode-map (kbd "C-c v c") 'haskell-cabal-visit-file))
+  '(progn
+     (define-key haskell-mode-map (kbd "C-c v c") 'haskell-cabal-visit-file)
+     (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+     (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+     (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+     (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+
+(custom-set-variables
+  '(haskell-process-type 'cabal-repl))
+
+;; https://github.com/haskell/haskell-mode/wiki/Haskell-Interactive-Mode-Tags
+
+;; (custom-set-variables
+;;   '(haskell-tags-on-save t))
+
+(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)
