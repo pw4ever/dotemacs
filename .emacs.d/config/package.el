3f897d40c7c045111a7ff666d7c4a0a2b7ae28f3
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-01-17 21:33:15 -0500

    use melpa-stable instead of marmalade as stable backup; the latter has TLS connection problem at this moment

--- .emacs.d/config/package.el
+++ .emacs.d/config/package.el
@@ -17,10 +17,10 @@
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 
-;; (add-to-list 'package-archives
-;; 	     '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
-
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+	     '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 (package-initialize)
