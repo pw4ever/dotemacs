93b61c892bc01be1c24bea7a38b1df80f9bec5e6
Wei pw Peng <write.to.peng.wei@gmail.com>
2015-01-25 13:10:35 -0500

    add (comint-clear-buffer) and bind to "C-c M-o" for other comint-based modes as in Cider

--- .emacs.d/config/post-package-load.el
+++ .emacs.d/config/post-package-load.el
@@ -6,3 +6,12 @@
 ;;(load-theme 'zenburn t)
 
 (load-theme 'monokai t)
+
+;; http://emacsredux.com/blog/2015/01/18/clear-comint-buffers/
+(defun comint-clear-buffer ()
+  (interactive)
+  (let ((comint-buffer-maximum-size 0))
+    (comint-truncate-buffer)))
+
+;; let's bind the new command to a keycombo
+(define-key comint-mode-map "\C-c\M-o" #'comint-clear-buffer)
