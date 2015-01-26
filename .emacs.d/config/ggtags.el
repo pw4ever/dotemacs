8c520ca7deb15f263ec43e91903bd4aa86a739fb
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-01-26 01:20:52 -0500

    change ggtags prefix key from \C-c to \C-c\C-t to reduce collision with, e.g., comint

--- .emacs.d/config/ggtags.el
+++ .emacs.d/config/ggtags.el
@@ -1,4 +1,8 @@
 ;; https://github.com/leoliu/ggtags
+(require 'ggtags)
+
+(custom-set-variables
+ (list 'ggtags-mode-prefix-key (kbd "C-c C-t")))
 
 (define-global-minor-mode global-ggtags-mode
   ggtags-mode
