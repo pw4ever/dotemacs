225f81ecebb005443978ad471944f57318f9eb5a
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-08-12 22:33:42 -0700

    Not enabling hideshow-minor-mode by default to prevent it from interfering [tab] completion.

--- .emacs.d/config/hideshow-org.el
+++ .emacs.d/config/hideshow-org.el
@@ -4,23 +4,25 @@
 (global-set-key (kbd "C-c @ TAB")
 		'hs-org/minor-mode)
 
-(dolist (hook '(lisp-hook-mode
-		emacs-lisp-mode-hook
+;; *not* enabling hideshow minor mode by default to prevent interfering [tab] completion
+
+;; (dolist (hook '(lisp-hook-mode
+;; 		emacs-lisp-mode-hook
 		
-		cider-mode-hook
-		cider-repl-mode-hook
-		clojure-mode-hook
+;; 		cider-mode-hook
+;; 		cider-repl-mode-hook
+;; 		clojure-mode-hook
                 
-		lisp-interaction-mode-hook
-		slime-repl-mode-hook
+;; 		lisp-interaction-mode-hook
+;; 		slime-repl-mode-hook
 
-		scheme-mode-hook
+;; 		scheme-mode-hook
 
-		ruby-mode-hook
+;; 		ruby-mode-hook
 
-		ess-mode-hook
-		ess-R-post-run-hook
+;; 		ess-mode-hook
+;; 		ess-R-post-run-hook
 
-		latex-mode-hook
-		LaTeX-mode-hook))
-  (add-hook hook (lambda () (hs-org/minor-mode))))
+;; 		latex-mode-hook
+;; 		LaTeX-mode-hook))
+;;   (add-hook hook (lambda () (hs-org/minor-mode))))
