9c6068797fd2fdfbbc10fe3c45905cd31000b4a6
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-08-19 09:39:33 -0700

    Bind common ggtags ops to shorter key combos.

--- .emacs.d/config/ggtags.el
+++ .emacs.d/config/ggtags.el
@@ -8,4 +8,16 @@
   ggtags-mode
   ggtags-mode)
 
+
+
+(eval-after-load 'ggtags
+  '(progn
+     (define-key ggtags-mode-map (kbd "M-*") 'ggtags-prev-mark)
+     (define-key ggtags-mode-map (kbd "M-[") 'ggtags-prev-mark)
+     (define-key ggtags-mode-map (kbd "M-{") 'ggtags-prev-mark)
+     (define-key ggtags-mode-map (kbd "M-}") 'ggtags-next-mark)
+     (define-key ggtags-mode-map (kbd "M-?") 'ggtags-show-definition)
+     (define-key ggtags-mode-map (kbd "M-/") 'ggtags-find-file)
+     ))
+
 (global-ggtags-mode)
