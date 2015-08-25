6dd6444c70428b9f1c6fe827c7d94d9a468cd4f8
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-08-25 09:37:58 -0700

    Tweak sr-speedbar-mode settings.

--- .emacs.d/config/sr-speedbar.el
+++ .emacs.d/config/sr-speedbar.el
@@ -1,6 +1,13 @@
 (require 'sr-speedbar)
-;;(sr-speedbar-open)
+
 (global-set-key '[f8] 'sr-speedbar-toggle)
+(global-set-key (kbd "M-_") 'sr-speedbar-toggle)
 
 ;; http://www.emacswiki.org/SrSpeedbar
-(setq speedbar-use-images nil)
+;;(setq speedbar-use-images nil)
+
+(let ((default 25))
+  (setq sr-speedbar-default-width default)
+  (setq sr-speedbar-width default))
+
+;;(sr-speedbar-open)
