78af0395f98908747dc19208450675106112357e
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-11-13 13:48:56 -0800

    Org Mode: Set up org-directory and org-mobile-directory at startup.

--- .emacs.d/config/org.el
+++ .emacs.d/config/org.el
@@ -4,3 +4,19 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+;; org-directory defaults to "$HOME/org/" if:
+;; * org-directory has not been set, i.e., is "nil",
+;; * $HOME/org is an accessible directory.
+(let ((home (getenv "HOME")))
+  (when (and (not (boundp 'org-directory)) (not org-directory) home)
+	(let ((org-home (concat (file-name-as-directory home)
+							"org")))
+	  (if (file-accessible-directory-p org-home)
+		  (setq org-directory org-home)))))
+
+(if (or (not (boundp 'org-mobile-directory))
+	   (not org-mobile-directory)
+	   (not (file-accessible-directory-p org-mobile-directory)))
+  (setq org-mobile-directory org-directory))
+
