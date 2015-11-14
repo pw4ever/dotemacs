fda731f1e773903194e6e982f84aeea878f1db13
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-11-13 22:27:43 -0800

    Add key-binding to point-undo/point-redo for use under terminal, in which C-, and C-. does not work.

--- .emacs.d/config/point-undo.el
+++ .emacs.d/config/point-undo.el
@@ -2,4 +2,6 @@
 
 (require 'point-undo)
 (global-set-key (kbd "C-,") 'point-undo)
+(global-set-key (kbd "M-[") 'point-undo)
 (global-set-key (kbd "C-.") 'point-redo)
+(global-set-key (kbd "M-]") 'point-redo)
