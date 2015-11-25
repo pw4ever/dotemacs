a75d4fad35d38eeb53144e9ae0b7356c984243e6
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-11-24 18:18:54 -0800

    Avoid inf-clojure and cider clash.

--- .emacs.d/config/inf-clojure.el
+++ .emacs.d/config/inf-clojure.el
@@ -1,4 +1,9 @@
 ;; http://github.com/clojure-emacs/inf-clojure
 
 (autoload 'inf-clojure "inf-clojure" "Run an inferior Clojure process" t)
-(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+
+;; Don't enable inf-clojure-minor-mode and cider-mode at the same time.
+;; They have overlapping functionality and keybindings and the result will be nothing short of havoc.
+;;(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+
+(add-hook 'inf-clojure-mode-hook #'eldoc-mode)
