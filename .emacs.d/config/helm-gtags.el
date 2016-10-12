f4e8c58e2e4a9391fa9922080127e28a2ce893b3
Wei Peng <write.to.peng.wei@gmail.com>
2016-10-12 16:44:05 -0700

    Tweak helm-gtags config.

--- .emacs.d/config/helm-gtags.el
+++ .emacs.d/config/helm-gtags.el
@@ -9,18 +9,21 @@
 (custom-set-variables
  '(helm-gtags-path-style 'relative)
  '(helm-gtags-ignore-case t)
- '(helm-gtags-auto-update t))
+ '(helm-gtags-auto-update nil))
 
 ;; key bindings
 (eval-after-load "helm-gtags"
   '(progn
 	 (global-set-key (kbd "<f6>") 'helm-gtags-mode)
 	 (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+	 (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-previous-history)
+	 (define-key helm-gtags-mode-map (kbd "C-M-,") 'helm-gtags-next-history)
      (define-key helm-gtags-mode-map (kbd "M-g M-t") 'helm-gtags-find-tag)
      (define-key helm-gtags-mode-map (kbd "M-g M-r") 'helm-gtags-find-rtag)
      (define-key helm-gtags-mode-map (kbd "M-g M-s") 'helm-gtags-find-symbol)
      (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
-     (define-key helm-gtags-mode-map (kbd "M-g [") 'helm-gtags-previous-history)
+     (define-key helm-gtags-mode-map (kbd "M-g M-u") 'helm-gtags-update-tags)
+	 (define-key helm-gtags-mode-map (kbd "M-g [") 'helm-gtags-previous-history)
      (define-key helm-gtags-mode-map (kbd "M-g ]") 'helm-gtags-next-history)
-     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+     
 	 (define-key helm-gtags-mode-map (kbd "M-/") 'helm-gtags-find-files)))
