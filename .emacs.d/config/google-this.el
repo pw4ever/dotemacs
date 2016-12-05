fecaacd1a65d2cc35594f66def6526ce370fd67f
Wei Peng <write.to.peng.wei@gmail.com>
2016-12-05 14:52:15 -0800

    Unmask "C-c /" for org-check-*.

--- .emacs.d/config/google-this.el
+++ .emacs.d/config/google-this.el
@@ -1,5 +1,8 @@
 ;; http://github.com/Malabarba/emacs-google-this
 
+;; Unmask "C-c /" for org-check-*
+(setq google-this-keybind (kbd "C-x g"))
+
 (require 'google-this)
 
 (add-hook 'text-mode-hook 'google-this-mode)
