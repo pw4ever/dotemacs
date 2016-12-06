d8608c8ef099060fe1098358732c78aa565c828a
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2016-12-05 20:31:14 -0800

    Unmask org-mode keys.

--- .emacs.d/config/0blayout.el
+++ .emacs.d/config/0blayout.el
@@ -1,4 +1,4 @@
 ;; Unmask "C-c C-l" for org-insert-link
-(0blayout-add-keybindings-with-prefix "C-c C-c C-l")
+(0blayout-add-keybindings-with-prefix "C-c =")
 
 (0blayout-mode)
--- .emacs.d/config/linum-relative.el
+++ .emacs.d/config/linum-relative.el
@@ -1,4 +1,4 @@
 (require 'linum-relative)
 
-(global-set-key (kbd "C-c C-l")
-				'linum-relative-toggle)
+;; (global-set-key (kbd "C-c C-l")
+;; 				'linum-relative-toggle)
