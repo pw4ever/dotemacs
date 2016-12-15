c09a0632990f4170d81e23700d2a7b0b1b455b18
Wei Peng <write.to.peng.wei@gmail.com>
2016-12-14 17:24:24 -0800

    xf86-lookup: Hack to make it encoding-system robust.

--- .emacs.d/config/x86-lookup.el
+++ .emacs.d/config/x86-lookup.el
@@ -17,3 +17,25 @@
 	  (when (and (file-exists-p default-x86-lookup-pdf)
 				 (not x86-lookup-pdf))
 		(setq x86-lookup-pdf default-x86-lookup-pdf)))))
+
+;; Hack the definition of `x86-lookup-create-index' to make sure the coding system is utf-8.
+(cl-defun x86-lookup-create-index (&optional (pdf x86-lookup-pdf))
+  "Create an index alist from PDF mapping mnemonics to page numbers.
+This function requires the pdftotext command line program."
+  (let ((mnemonic (concat "INSTRUCTION SET REFERENCE, [A-Z]-[A-Z]\n\n"
+                          "\\([[:alnum:]/ ]+\\)[- ]?—"))
+		(coding-system-for-read 'utf-8)
+		(coding-system-for-write 'utf-8)
+        (case-fold-search nil))
+    (with-temp-buffer
+      (call-process x86-lookup-pdftotext-program nil t nil
+                    (file-truename pdf) "-")
+      (setf (point) (point-min))
+      (cl-loop for page upfrom 1
+               while (< (point) (point-max))
+               when (looking-at mnemonic)
+               nconc (x86-lookup--expand (match-string 1) page) into index
+               do (forward-page)
+               finally (cl-return
+                        (cl-remove-duplicates
+                         index :key #'car :test #'string= :from-end t))))))
