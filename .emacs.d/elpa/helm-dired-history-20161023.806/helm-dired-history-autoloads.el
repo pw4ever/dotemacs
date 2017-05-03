2849658ec83a6dfa8c2ea1819fce33c56d1cb15b
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2017-05-02 17:02:51 -0700

    Update ELPA.

--- .emacs.d/elpa/helm-dired-history-20161023.806/helm-dired-history-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-dired-history-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-dired-history" "helm-dired-history.el"
-;;;;;;  (22547 54355 458680 211000))
-;;; Generated autoloads from helm-dired-history.el
-
-(autoload 'helm-dired-history-view "helm-dired-history" "\
-call `helm' to show dired history.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-dired-history-autoloads.el ends here
