3cc14164dd8c3fcdf6cf1f60e5c56154c90aaacb
Wei Peng <write.to.peng.wei@gmail.com>
2017-06-09 15:51:07 -0700

    Add xahk-mode for editing AutoHotkey scripts (.ahk).

---
+++ .emacs.d/elpa/xahk-mode-20170523.1946/xahk-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xahk-mode" "20170523.1946" "Major mode for editing AutoHotkey scripts." '((emacs "24.1")) :commit "538b891a1565d109d079185b56332cd28dd846a7" :url "http://xahlee.info/mswin/emacs_autohotkey_mode.html" :keywords '("languages"))
