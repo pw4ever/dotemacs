a988256f34f3b331425f4e91f69bb62069c85ed7
Wei Peng <write.to.peng.wei@gmail.com>
2017-07-01 01:12:39 -0700

    Refactor theme loading code into a separate file, and make server-ensure-safe-dir a nop on windows-nt.

---
+++ .emacs.d/config/post-package-load.d/theme.el
@@ -0,0 +1,24 @@
+;;
+;; Load $HOME/theme.el, which should contain a single "load-theme" funcall, if it exists.
+;; This allowes customization without touching the version-controlled config.
+;;
+
+;; color themes:
+;; - adwaita
+;; - solarized-light
+;; - solarized-dark
+;; - zenburn
+;; - monokai
+(let* ((default-theme 'monokai)
+       (home (getenv "HOME"))
+       (theme-file-name (and home
+                             (concat (file-name-as-directory home)
+                                     "theme.el")))
+       (theme-file (and theme-file-name
+                        (file-exists-p theme-file-name)
+                        theme-file-name)))
+  (if theme-file
+      (load-file theme-file)
+    (load-theme default-theme t)))
+
+(setq custom-safe-themes t)             ; Consider all themes as safe.
--- .emacs.d/config/post-package-load.el
+++ .emacs.d/config/post-package-load.el
@@ -1,26 +1,3 @@
-;;
-;; Load $HOME/theme.el, which should contain a single "load-theme" funcall, if it exists.
-;; This allowes customization without touching the version-controlled config.
-;;
-
-;; color themes:
-;; - adwaita
-;; - solarized-light
-;; - solarized-dark
-;; - zenburn
-;; - monokai
-(let* ((default-theme 'monokai)
-       (home (getenv "HOME"))
-       (theme-file-name (and home
-                             (concat (file-name-as-directory home)
-                                     "theme.el")))
-       (theme-file (and theme-file-name
-                        (file-exists-p theme-file-name)
-                        theme-file-name)))
-  (if theme-file
-      (load-file theme-file)
-    (load-theme default-theme t)))
-
 ;;; clear comint buffer
 ;; http://emacsredux.com/blog/2015/01/18/clear-comint-buffers/
 (defun comint-clear-buffer ()
@@ -62,3 +39,8 @@
 
 (defvaralias 'c-basic-offset 'tab-width)
 (defvaralias 'cperl-indent-level 'tab-width)
+
+(if (eq system-type 'windows-nt)
+    (defun server-ensure-safe-dir (dir)
+      "Nop"
+      t))
