5963aa0ac856a65121d410b3a4f67a3788dfbf01
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2017-09-11 16:35:42 -0700

    Since flyspell-mode hangs Emacs on startup on Windows, do not enable it by default.

--- .emacs.d/config/pre-package-load.d/flyspell.el
+++ .emacs.d/config/pre-package-load.d/flyspell.el
@@ -1,7 +1,8 @@
 ;; flyspell mode
 ;; http://www.emacswiki.org/emacs/FlySpell
-(add-hook 'text-mode-hook 'flyspell-mode)
+;;(add-hook 'text-mode-hook 'flyspell-mode)
 (setq flyspell-use-meta-tab nil)		; prevent it from clashing with, e.g., Org's M-TAB
+(setq flyspell-issue-welcome-flag nil)
 (setq flyspell-issue-message-flag nil)
 (eval-after-load "helm"
   '(progn
