e3fe5583f428ac7a917ea93b184fe35a541cc056
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2017-10-25 19:12:09 -0700

    Turn on the hybrid relative linum mode.

--- .emacs.d/config/linum-relative.el
+++ .emacs.d/config/linum-relative.el
@@ -1,4 +1,8 @@
 (require 'linum-relative)
 
+;; https://emacs.stackexchange.com/a/19541
+(setq linum-relative-current-symbol "")
+
+(linum-relative-global-mode +1)
 ;; (global-set-key (kbd "C-c C-l")
 ;; 				'linum-relative-toggle)
