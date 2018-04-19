165a23b1d3bfedf0c98103399482c54e9d325598
Wei Peng <4pengw+github@gmail.com>
2018-04-19 16:06:24 -0700

    Upgrade packages.

--- .emacs.d/elpa/inf-clojure-20180316.1140/inf-clojure-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "inf-clojure" "20180316.1140" "Run an external Clojure process in an Emacs buffer" '((emacs "24.4") (clojure-mode "5.6")) :commit "69137983d368af0dc76cfa2e9e33338b796df5f4" :url "http://github.com/clojure-emacs/inf-clojure" :keywords '("processes" "clojure"))
