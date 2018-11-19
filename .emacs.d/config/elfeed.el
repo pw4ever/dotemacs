7b2b8db7840a65b1e08df6172ea1e4c9f4cab8fa
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2018-11-18 17:26:51 -0800

    Bind "C-x w f" to elfeed.

---
+++ .emacs.d/config/elfeed.el
@@ -0,0 +1,11 @@
+;; https://github.com/skeeto/elfeed
+(global-set-key (kbd "C-x w f") 'elfeed) ; allow C-x w to be used for
+
+;;
+;; Setting up further feeds. Recommended: ~/.emacs.d/local.d/elfeed.el
+;;
+;; (append-to-list 'elfeed-feeds
+;;                 '(("http://brendangregg.com/blog/rss.xml" perf system personal)
+;;                   ("https://sourceware.org/git/gitweb.cgi?p=systemtap.git;a=rss" perf systemtap)
+;;                   ("http://nullprogram.com/feed/" programming personal)
+;;                   ("https://www.reddit.com/r/kernel/.rss" linux kernel)))
