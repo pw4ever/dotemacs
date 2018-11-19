d9afb5a5fab8fcc686b1729eee86786d4c5ff448
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2018-11-18 16:19:42 -0800

    Add elfeed and friends.

---
+++ .emacs.d/elpa/elfeed-protocol-20181117.1159/elfeed-protocol-pkg.el
@@ -0,0 +1,15 @@
+(define-package "elfeed-protocol" "20181117.1159" "Provide owncloud/ttrss protocols for elfeed"
+  '((emacs "24.4")
+    (elfeed "2.1.1")
+    (cl-lib "0.5")
+    (auth-source "0"))
+  :keywords
+  '("news")
+  :authors
+  '(("Xu Fasheng" . "fasheng.xu@gmail.com"))
+  :maintainer
+  '("Xu Fasheng" . "fasheng.xu@gmail.com")
+  :url "https://github.com/fasheng/elfeed-protocol")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
