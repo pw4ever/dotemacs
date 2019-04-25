f2fd85ad43c3f18ea9f74d9c987816a6880c3c87
Wei Peng <write.to.peng.wei@gmail.com>
2019-04-24 17:48:14 -0700

    Upgrade packages.

--- .emacs.d/elpa/rtags-20190313.656/rtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rtags" "20190313.656" "A front-end for rtags" 'nil :commit "515e31f57cb77d2c80eae840cf176438ef2fe88b" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
