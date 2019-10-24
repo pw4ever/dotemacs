fe94f3e81d67aa889016ddf8fc27f92218130d68
Wei Peng <4pengw+github@gmail.com>
2019-10-23 23:04:28 -0700

    Update post-package-load workarounds.

--- .emacs.d/config/post-package-load.d/zz99_workarounds.el
+++ .emacs.d/config/post-package-load.d/zz99_workarounds.el
@@ -1,2 +1,3 @@
 ;; https://github.com/emacs-evil/evil/issues/1074#issuecomment-430060079
-(setq undo-tree-enable-undo-in-region nil)
+(global-undo-tree-mode -1)
+;;(setq undo-tree-enable-undo-in-region nil)
