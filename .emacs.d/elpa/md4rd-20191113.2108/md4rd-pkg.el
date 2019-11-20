52c95d394bee9681375accd5fcf82b9b56d5e600
Wei Peng <4pengw+github@gmail.com>
2019-11-20 14:58:25 -0800

    Upgrade packages.

---
+++ .emacs.d/elpa/md4rd-20191113.2108/md4rd-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "md4rd" "20191113.2108" "Mode for reddit (browse it)." '((emacs "25.1") (hierarchy "0.7.0") (request "0.3.0") (cl-lib "0.6.1") (dash "2.12.0") (s "1.12.0") (tree-mode "1.0.0")) :commit "ff7d95fc0fdf46a3cb6b3bbe52396dc4e474b8fd" :keywords '("ahungry" "reddit" "browse" "news") :authors '(("Matthew Carter" . "m@ahungry.com")) :maintainer '("Matthew Carter" . "m@ahungry.com") :url "https://github.com/ahungry/md4rd")
