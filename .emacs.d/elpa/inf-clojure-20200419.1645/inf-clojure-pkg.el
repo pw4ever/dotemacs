aa0c964a362da88c8e84aa2272803ff8edf62595
Wei Peng <4pengw+github@gmail.com>
2020-04-24 17:41:56 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/inf-clojure-20200419.1645/inf-clojure-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-clojure" "20200419.1645" "Run an external Clojure process in an Emacs buffer" '((emacs "24.4") (clojure-mode "5.11")) :commit "fe22ce2bd6848e9ed69e8349c518f90a79a5c101" :keywords '("processes" "clojure") :url "http://github.com/clojure-emacs/inf-clojure")
