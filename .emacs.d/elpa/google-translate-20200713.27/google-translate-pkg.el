fcc5a2707cd75043adf2ff20bc08cae6258e2391
Wei Peng <4pengw+git@gmail.com>
2020-07-19 12:23:37 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/google-translate-20200713.27/google-translate-pkg.el
@@ -0,0 +1,4 @@
+(define-package "google-translate" "20200713.27" "Emacs interface to Google Translate." 'nil :commit "c66cbc98f33cd602e710084d76cda7473cb493d3")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
