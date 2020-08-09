ba42aeee31bfa89b24b259ad6cfe17fe7333f20d
Wei Peng <4pengw+github@gmail.com>
2020-08-09 12:07:56 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/clojure-essential-ref-nov-20200719.608/clojure-essential-ref-nov-autoloads.el
@@ -0,0 +1,23 @@
+;;; clojure-essential-ref-nov-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "clojure-essential-ref-nov" "clojure-essential-ref-nov.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from clojure-essential-ref-nov.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "clojure-essential-ref-nov" '("clojure-essential-ref-nov")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; clojure-essential-ref-nov-autoloads.el ends here
---
+++ .emacs.d/elpa/clojure-essential-ref-nov-20200719.608/clojure-essential-ref-nov-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-essential-ref-nov" "20200719.608" "Cider-doc to \"Clojure, The Essential Reference\" (EPUB)" '((emacs "24") (dash "2.16.0") (nov "0.3.1") (clojure-essential-ref "0.1.0")) :commit "e05d61b96f6469a93f52015b7ad5deadf616139e" :url "https://github.com/p3r7/clojure-essential-ref")
