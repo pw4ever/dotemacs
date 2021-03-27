064053f9486c21ad3368caa58e103ae1f3005549
Wei Peng <me@1pengw.com>
2021-03-27 14:27:21 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/clojure-mode-extra-font-locking-20210322.704/clojure-mode-extra-font-locking-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from clojure-mode-extra-font-locking.el  -*- no-byte-compile: t -*-
+(define-package "clojure-mode-extra-font-locking" "20210322.704" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "a14671e03c867c9d759ee9e59cdc5cecbf271245" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :keywords '("languages" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
