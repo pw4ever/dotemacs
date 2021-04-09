f234ee88f4ed8d5b028c71e994a82a7ce376b58a
Wei Peng <me@1pengw.com>
2021-04-09 14:02:43 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/elfeed-protocol-20210401.100/elfeed-protocol-pkg.el
@@ -0,0 +1,14 @@
+(define-package "elfeed-protocol" "20210401.100" "Provide fever/newsblur/owncloud/ttrss protocols for elfeed"
+  '((emacs "24.4")
+    (elfeed "2.1.1")
+    (cl-lib "0.5"))
+  :commit "2b2aaf2f3b92e7c27827e0f280598cb52db558e0" :authors
+  '(("Xu Fasheng <fasheng[AT]fasheng.info>"))
+  :maintainer
+  '("Xu Fasheng <fasheng[AT]fasheng.info>")
+  :keywords
+  '("news")
+  :url "https://github.com/fasheng/elfeed-protocol")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
