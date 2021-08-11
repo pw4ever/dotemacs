53d1fcb27dfc8d01e5862459af31e9cc13372035
Wei Peng <me@1pengw.com>
2021-08-11 15:05:11 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/ess-smart-equals-20210411.1333/ess-smart-equals-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ess-smart-equals.el  -*- no-byte-compile: t -*-
+(define-package "ess-smart-equals" "20210411.1333" "flexible, context-sensitive assignment key for R/S" '((emacs "25.1") (ess "18.10")) :commit "fea9eea4b59c3e9559b379508e3500076ca99ef1" :authors '(("Christopher R. Genovese" . "genovese@cmu.edu")) :maintainer '("Christopher R. Genovese" . "genovese@cmu.edu") :keywords '("r" "s" "ess" "convenience") :url "https://github.com/genovese/ess-smart-equals")
