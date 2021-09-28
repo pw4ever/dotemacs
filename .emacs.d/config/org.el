7a1eda6b0c78e800852e4113d658e763bc9fdceb
Wei Peng <me@1pengw.com>
2021-09-28 15:04:18 -0700

    org: require explicit {} for super/sub-scripting.

--- .emacs.d/config/org.el
+++ .emacs.d/config/org.el
@@ -12,6 +12,7 @@
 (setq org-enforce-todo-dependencies t)
 (setq org-default-notes-file "~/notes.org")
 (setq org-todo-keywords '((sequence "TODO(t)" "IN-PROGRESS(i)" "HOLD(h)" "|" "DONE(d)" "CANCELED(r)")))
+(setq org-use-sub-superscripts '{})
 
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
