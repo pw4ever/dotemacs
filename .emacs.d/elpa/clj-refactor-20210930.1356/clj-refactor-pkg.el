9bed23eae0c221d5ad4ba8541345477c0573fc10
Wei Peng <me@1pengw.com>
2021-09-30 12:57:36 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/clj-refactor-20210930.1356/clj-refactor-pkg.el
@@ -0,0 +1,23 @@
+(define-package "clj-refactor" "20210930.1356" "A collection of commands for refactoring Clojure code"
+  '((emacs "26.1")
+    (seq "2.19")
+    (yasnippet "0.6.1")
+    (paredit "24")
+    (multiple-cursors "1.2.2")
+    (clojure-mode "5.9")
+    (cider "1.0")
+    (parseedn "1.0.4")
+    (inflections "2.3")
+    (hydra "0.13.2"))
+  :commit "ff8877ac80efe843fae116ae1e393a5145dd11ac" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com")
+    ("Lars Andersen" . "expez@expez.com")
+    ("Benedek Fazekas" . "benedek.fazekas@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("convenience" "clojure" "cider"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
