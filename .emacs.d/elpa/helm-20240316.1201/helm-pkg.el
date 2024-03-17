78fa23e62c1dfad87d35cd60ef3cc66c43ecccff
Wei Peng <me@1pengw.com>
2024-03-16 21:51:24 -0700

    Upgrade packages.

---
+++ .emacs.d/elpa/helm-20240316.1201/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20240316.1201" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.7")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "e93335b0b9488de4af97058cd65e3114ebd1bf3e" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
